66a135af014569a6bb437d4990f3191f99827601
Rudin <rerofya@gmail.com>
2025-10-04 16:06:38 +0700

    change title modal

--- app/Views/pages/admin/employee/edit.php
+++ app/Views/pages/admin/employee/edit.php
@@ -7,7 +7,7 @@
     <div class="modal-dialog">
         <form method="POST" action="<?= base_url('admin/employee/update/' . $data['id']); ?>" class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="addModalLabel">Edit Departement</h1>
+                <h1 class="modal-title fs-5" id="addModalLabel">Edit Employee</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
